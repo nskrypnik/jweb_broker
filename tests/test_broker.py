9107700813425181dcc2be83c24ad4cad204cae0
Niko Skrypnik <nskrypnik@gmail.com>
2017-05-05 13:00:20 -0700

    Add more unitttests

--- tests/test_broker.py
+++ tests/test_broker.py
@@ -1,24 +1,41 @@
-from utils import patch_all, MockedDBClient
+from utils import patch_all, MockedDBClient, AsyncMock
 patch_all()
 
 import asyncio
 import unittest
-from cefpython3 import cefpython as cef
+from unittest.mock import MagicMock, patch
 from jweb_broker import Broker
 from jweb_broker.tasks import BaseTasksLookout
+from jweb_broker.workers_pool import WorkersPool
 
 
 class MockTasksLookout(BaseTasksLookout):
     pass
 
 
+class MockedLoop:
+
+    def __init__(self, *args, **kw):
+        self.create_task = MagicMock()
+
+
 class TestBroker(unittest.TestCase):
 
     def setUp(self):
         self.loop = asyncio.get_event_loop()
-
-    def tearDown(self):
-        self.loop.close()
+        self.broker = Broker(
+            loop=self.loop,
+            db_name='test',
+            tasks_lookout=MockTasksLookout()
+        )
+        loop = MockedLoop()
+        self.fake_loop_broker = Broker(
+            loop=loop,
+            db_name='test',
+            tasks_lookout=MockTasksLookout(),
+            drop_jobs_collection=True
+        )
+        self.fake_loop = loop
 
     def test_init(self):
         # should throuw an exception when db_name is not provided
@@ -35,3 +52,26 @@ class TestBroker(unittest.TestCase):
 
         broker = Broker(loop=self.loop, db_name='test', tasks_lookout=MockTasksLookout())
         self.assertTrue(isinstance(broker.db_client, MockedDBClient))
+
+    def test_init_workers_pool(self):
+        self.assertTrue(isinstance(self.broker.workers_pool, WorkersPool))
+
+    def test_launch_jobs_manager(self):
+
+        self.loop.run_until_complete(self.fake_loop_broker.launch_jobs_manager())
+        self.fake_loop_broker.db.drop_collection.assert_called()
+        self.fake_loop_broker.db.create_collection.assert_called()
+        self.fake_loop_broker.jobs_collection.create_index.assert_called()
+        self.fake_loop.create_task.assert_called()
+
+    def test_run_jobs_manager(self):
+        t = self.broker.update_job_state
+        self.broker.update_job_state = AsyncMock(return_value=dict(test='test'))
+        self.loop.create_task(self.broker.run_jobs_manager())
+
+        async def check_job_data():
+            job_data = await self.broker.jobs_queue.get()
+            self.assertEqual(job_data['test'], 'test')
+
+        self.loop.run_until_complete(check_job_data())
+        self.broker.update_job_state = t
--- tests/utils.py
+++ tests/utils.py
@@ -1,8 +1,19 @@
+import asyncio
 from unittest.mock import MagicMock
+
+
+class AsyncMock(MagicMock):
+    async def __call__(self, *args, **kwargs):
+        return super(AsyncMock, self).__call__(*args, **kwargs)
 
 
 class MockedBrowserDriver:
     pass
+
+
+class MockCursor:
+    def __init__(self, *args, **kw):
+        self.to_list = AsyncMock(return_value=[{}, ])
 
 
 class MockedDriversPool:
@@ -12,11 +23,18 @@ class MockedDriversPool:
 
 
 class MockedDBCollection:
-    pass
+    def __init__(self, *args, **kw):
+        self.create_index = AsyncMock()
+
+    def find(self, *args, **kw):
+        return MockCursor()
 
 
 class MockedDB(dict):
-    pass
+
+    def __init__(self, *args, **kw):
+        self.drop_collection = AsyncMock()
+        self.create_collection = AsyncMock()
 
     def __getitem__(self, name):
         return MockedDBCollection()
